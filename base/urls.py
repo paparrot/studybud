2f91a753d12bc1e7167132a0b3b66a654aba3542
Klim <subbotin.klim@gmail.com>
2023-02-24 16:09:16 +0100

    add rooms CRUD functionality from user unterface

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('rooms/', views.rooms, name="rooms"),
-    path('rooms/<int:id>', views.room, name="room")
+    path('rooms/<int:id>', views.room, name="room"),
+    path('rooms/create', views.create_room, name="create-room"),
+    path('rooms/<int:id>/update', views.update_room, name="update-room"),
+    path('rooms/<int:id>/delete', views.delete_room, name="delete-room")
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from django.http import HttpResponse
+from .forms import RoomForm
 
 
 # Create your views here.
@@ -22,3 +23,42 @@ def room(request, id):
 
     context = {'room': _room}
     return render(request, 'room.html', context)
+
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rooms')
+
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
+
+
+def update_room(request, id):
+    room = Room.objects.get(id=id)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('rooms')
+
+    context = {
+        'room': room,
+        'form': form,
+    }
+
+    return render(request, 'room_form.html', context)
+
+def delete_room(request, id):
+    room = Room.objects.get(id=id)
+
+    if request.method == "POST":
+        room.delete()
+        return redirect('rooms')
+
+    return render(request, 'delete.html', { 'object': room})
