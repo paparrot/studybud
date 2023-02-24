714207af13d8285bb86a22b7513e2ad3252ef8f8
Klim <subbotin.klim@gmail.com>
2023-02-24 15:22:46 +0100

    add models and migration for Topic and Message entity

--- base/models.py
+++ base/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    """Room topic model"""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
 class Room(models.Model):
     """Room model"""
-    # host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(null=True, blank=True)
     # participants =
@@ -14,3 +23,15 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    """User message model"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
